ab59e056d815a30197738879c8844fb9e7ff8a19
Aaron Fowler <aaronwfowler@gmail.com>
2022-06-11 22:44:02 +0200

    Built on draft of TransitSystem class and the TransitRoute classes.  Need to re-visit how to handle LinkedList with routes that re-use transit stops that are used by other transit routes.

--- main.py
+++ main.py
@@ -3,20 +3,26 @@
 #
 #
 
+class PickAUniqueName(Exception):
+    """
+    A custom error class for when a route is created, but the name is already in use.
+    """
+
+    pass
 
 class TransitStopNode:
     """
     TODO
     """
-    def __init__(self, stop_name, layover=False):
+    def __init__(self, route_name, stop_name, stop_order):
         self._stop_name = stop_name
-        self._stop_order = {}
+        self._stop_order = {route_name: stop_order}
         self._stop_direction = {}
         self._location = {"lat": None, "long": None, "address": None}
         self._ridership = None
         self._amenities = None
         self._routes = []
-        self._layover = layover
+        self._layover = False
         self.next_stop = None
 
     def __repr__(self):
@@ -61,3 +67,138 @@ class TransitStopNode:
 
         return self._layover
 
+    def set_layover(self, bool_value):
+        """
+        TODO
+        """
+
+        self._layover = bool_value
+
+    def add_route(self, route_obj):
+        """
+        TODO
+        """
+
+        self._routes.append(route_obj)
+
+    def get_routes(self):
+        """
+        TODO
+        """
+
+        return self._routes
+
+    def get_lat_location(self):
+        """
+        TODO
+        """
+
+        return self._location["lat"]
+
+    def get_long_location(self):
+        """
+        TODO
+        """
+
+        return self._location["long"]
+
+    def get_address_location(self):
+        """
+        TODO
+        """
+
+        return self._location["address"]
+
+    def set_lat_location(self, lat_float):
+        """
+        TODO
+        """
+
+        self._location["lat"] = lat_float
+
+    def set_long_location(self, long_float):
+        """
+        TODO
+        """
+
+        self._location["long"] = long_float
+
+    def set_address_location(self, address_string):
+        """
+        TODO
+        """
+
+        self._location["address"] = address_string
+
+
+
+class TransitSystem:
+    """
+    TODO
+    """
+    def __init__(self, system_name):
+        self._route_dictionary = {}  # Route name as key, Route object as value
+        self._system_name = system_name
+
+
+
+    def create_route(self, route_name, stop_name_list):
+        """
+        TODO
+        """
+        if route_name not in self._route_dictionary.keys():
+            return
+
+        else:
+            raise PickAUniqueName("Sorry! "+str(route_name)+" is already in use!  Please pick a unique route name.")
+
+        #return
+
+
+class TransitRoute:
+    """
+    TODO
+    """
+    def __init__(self, route_name, circulator, stop_names_list):
+        self._route_name = route_name
+        self._circulator = circulator
+        self._stop_names = stop_names_list
+        self._stop_dictionary = {}
+        self._first_stop = None
+
+    def create_stops(self, route_name, stop_name_list):
+        """
+        TODO
+        """
+
+        self._first_stop = TransitStopNode(route_name, stop_name=stop_name_list[0], stop_order=0)
+        current = self._first_stop
+        self._X_dictionary[current.get_stop_name()] = current
+
+        for indx in range(1,len(stop_name_list)):
+            current.next_stop = TransitStopNode(route_name, stop_name=stop_name_list[0], stop_order=indx)
+            current = current.get_next_stop()
+            self._X_dictionary[current.get_stop_name()] = current
+
+        current.next_stop = self._first_stop
+
+    def get_route_name(self):
+        """
+        TODO
+        """
+
+        return self._route_name
+
+    def get_circulator(self):
+        """
+        TODO
+        """
+
+        return self._circulator
+
+    def set_circulator(self, bool_value):
+        """
+        TODO
+        """
+
+        self._circulator = bool_value
